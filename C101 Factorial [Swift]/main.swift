6ffebd021096a40903ff7d7d4ee847227ae0e0f3
Zoe Yau <zoe.yau@student.allenisd.org>
2022-11-17 23:03:52 -0600

    Rewrite

--- C101 Factorial [Swift]/main.swift
+++ C101 Factorial [Swift]/main.swift
@@ -1,4 +1,3 @@
-
 // ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
 // DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
 let n = 7
@@ -8,23 +7,34 @@ let n = 7
 
 // Add your code below:
 
+//Function that calculates factorials
+//Parameters: Integer & Returns: Integer
 func factorial(_ n: Int) -> Int {
 
+    //Prints how many times the factorial is being calculated
     print("Calculating factorial(\(n))...")
 
+    //Runs if only there is less than or equal to 1 in n 
     if n <= 1 {
+        //1 will always be the terminal case because it is the starting number
         print("Terminal case, returning 1.")
         return 1
 
+    //If n > 1 this code will run
     } else {
+        //Where the factorial is calculated
+        //Factorial starts with 1 then multiplies it by 2 then multiples that number by 3 then that number by 4 and so on...
+        //Ex. 1, 2 * 1, 3 * 2 * 1, 4 * 3 * 2 * 1
         let result = n * factorial(n - 1)
+        //Prints the result of the factorial calculation
         print("Non-terminal case, returning \(result).")
         return result
     }
     
 }
 
+//Starts factorials at 1 then 2 then 3 etc. and keeps calcuating the factorial of n number of times
+//Prints the last factorial calculated 
 print("The final result is: \(factorial(n)).")
-
 
 
--- C102 Fibonacci [Swift]/main.swift
+++ C102 Fibonacci [Swift]/main.swift
@@ -6,16 +6,25 @@ let n = 7
 
 // Add your code below:
 
+//Function that calculates the fibonacci sequence
+//Parameters: Int & Returns: Int
 func fib(n: Int) -> Int {
 
+    //Prints the number of times fibonacci will be calculated 
     print("Calculating fibonacci(\(n))...")
     
+    //Only runs if n is less than or equal to 2 
     if n <= 2 {
+        //The first case will always be 1 
         print("Terminal case, returning 1.")
         return 1
 
     } else {
+        //Calculates the fibonacci sequence
+        //Starts with 1 + 1 then adds the previous answer to the second digit in the equation
+        //Ex. 1 + 1, 1 + 2, 2 + 3, 3 + 5
         let result = fib(n: n - 1) + fib(n: n - 2)
+        //Prints the result of the calculation 
         print("Non-terminal case, returning \(result).")
         return result
 
@@ -23,4 +32,6 @@ func fib(n: Int) -> Int {
 
 }
 
+//Starts at 1 and continues to calculate the sequence n number of times
+//Prints the last number in the fibonacci sequence calculated
 print("The final result is: \(fib(n: n)).")
